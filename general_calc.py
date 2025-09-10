d5da3249ab5912d24c4a6798ada954c7e49bcbc4
TOXYGENCY <delorean2026@ya.ru>
2025-09-10 23:54:40 +0300

    Добавлены (почти) все операции из списка. Добавлен унарный минус к списку операций и его обработка.

--- general_calc.py
+++ general_calc.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from math import log, sin, cos, tan, radians
 
 # Константы (poor man's config)
 WINDOW_TITLE = "Калькулятор"
@@ -10,25 +11,37 @@ VALIDATION_ERROR_TITLE = "Ошибка ввода"
 
 
 # Переменные
+
+# Все ("названия":"внутренние_значения") кнопок. Для каждой создается отдельная кнопка
+# При парсинге изначальной строки названия будут заменяться на внутренние_значения
+# Внутренние значения нужны для простого парсинга операций по двум знакам
+button_labels = [
+    [("Del", "D"), ("(", "("), (")", ")"), ("*", "*")],
+    [("7", "7"), ("8", "8"), ("9", "9"), ("/", "/")],
+    [("4", "4"), ("5", "5"), ("6", "6"), ("-", "-")],
+    [("1", "1"), ("2", "2"), ("3", "3"), ("+", "+")],
+    [("C", "C"), ("0", "0"), ("=", "="), ("|x|", "|")],
+    [("log_e(x)", "ln"), ("^", "^"), ("sqrt(x)", "sq"), ("=", "=")],
+    [("sin(x)", "sn"), ("cos(x)", "cs"), ("tan(x)", "tn"), ("ctan(x)", "ct")],
+]
 num_buffer = ""  # Буфер для учета многозначных чисел
 notation = []  # Стек с обратной польской нотацией
-operator_stack = []  # Стек с операторами
-operators = {  # словарь операторов и их приоритетов
+operation_stack = []  # Стек с операторами
+operations = {  # словарь операций и их приоритетов
+    "--": 3,  # Явный унарный минус
+    "^": 2,
+    "sq": 2,  # sqrt
+    "ln": 1,  # log_e
+    "sn": 1,  # sin
+    "cs": 1,  # cos
+    "tn": 1,  # tan
+    "ct": 1,  # ctan
     "*": 1,
     "/": 1,
     "+": 0,
     "-": 0,
 }
-operations = {  # словарь операций и их приоритетов
-    "**": 1,
-    "e^": 1,
-    "sqrt": 1,
-    "log_e": 0,
-    "sin": 0,
-    "cos": 0,
-    "tan": 0,
-    "ctan": 0,
-}
+
 
 # Проверяет является ли X числом
 def is_int(x):
@@ -49,16 +62,43 @@ def save_number():
     print("Number saved. Buffer cleared.")
 
 
+# Конвертация строки ввода в укороченный стандартизированный вид перед обработкой
+def convert_symbols(text):
+    text = str(text).replace("x", "1", -1)  # допущение
+    text = str(text).replace("**", "^", -1)
+    text = str(text).replace("sqrt", "sq", -1)
+    text = str(text).replace("log_e", "ln", -1)
+    text = str(text).replace("sin", "sn", -1)
+    text = str(text).replace("cos", "cs", -1)
+    text = str(text).replace("tan", "tn", -1)
+    text = str(text).replace("ctan", "ct", -1)
+    text = str(text).replace(" ", "", -1)
+    return text
+
+
+# Cокращение проверки на спец символ для условий в parse_input()
+def is_special(current, last):
+    current = str(current)
+    last = str(last)
+    is_oper = str(current + last) in operations
+    is_bracket = str(current + last) in ["(", ")", "|"]
+    return is_oper or is_bracket
+
+
 # Основная функция парсинга инфиксной записи в ОПН
 def parse_input():
-    global num_buffer, operator_stack, operators, notation
+    global num_buffer, operation_stack, operations, notation
     # Очищаем рабочие переменные
-    notation, operator_stack, num_buffer = [], [], ""
+    notation, operation_stack, num_buffer = [], [], ""
 
-    input = input_entry.get()
+    input = convert_symbols(input_entry.get())
+
     print("Processing:", input)
-    for i in range(len(input)):
+    i = 0
+    # Из-за изменения строки input, нельзя использовать фиксированный for
+    while i < len(input):
         symb = input[i]
+        print("--processing:", symb)
 
         # Числа добавляем в буфер
         if is_int(symb):
@@ -67,51 +107,69 @@ def parse_input():
             if i == len(input) - 1:
                 save_number()
 
+        # Унарный минус. Либо по контексту, либо явный "--"
+        elif (
+            ((i < len(input) - 1) and (symb == "-" == str(symb + input[i + 1])))
+            or ((i == 0) and (symb == "-"))
+            or (
+                (0 < i < len(input) - 1)
+                and (symb == "-")
+                and (is_special(symb, input[i - 1]))
+            )
+        ):
+            save_number()
+            # Извращение чтобы задублировать унарный минус из "-" в явный "--"
+            # вставка доп "-" в разрыве
+            # input = input[0:i:1] + "-" + input[i + 1 : -1 : 1]
+            operation_stack.append("--")
+
         # Откр. скобка в стек
         elif symb == "(":
             save_number()
-            operator_stack.append(symb)
+            operation_stack.append(symb)
 
-        # Добавляем операции в стек
-        elif i != len(input) and str(symb + input[i + 1]) in operations:
+        # Добавляем операции в стек. Проверяем по двум символам
+        elif i < len(input) - 1 and str(symb + input[i + 1]) in operations:
             save_number()
-            operator_stack.append(symb)
+            operation_stack.append(str(symb + input[i + 1]))
 
         # Добавляем операторы в стек в порядке приоритетов
-        elif symb in operators:
+        elif symb in operations:
             save_number()
             oper = symb
             # Пока видим операторы в высшим приоритетом - переносим их в нотацию
             while (
-                len(operator_stack) > 0
-                and operator_stack[-1] != "("  # вытаскиваем до откр. скобки
-                and operators[oper] <= operators[operator_stack[-1]]
+                len(operation_stack) > 0
+                and operation_stack[-1] != "("  # вытаскиваем до откр. скобки
+                and operations[oper] <= operations[operation_stack[-1]]
             ):
-                notation.append(operator_stack.pop())
-            operator_stack.append(symb)
+                notation.append(operation_stack.pop())
+            operation_stack.append(symb)
 
         # По закрытии скобок переносим в нотацию все внутренние (упорядоченные) операторы
         elif symb == ")":
             save_number()
-            while len(operator_stack) > 0 and operator_stack[-1] != "(":
-                notation.append(operator_stack.pop())
-            operator_stack.pop()  # удаление оставшейся открывающей скобки
+            while len(operation_stack) > 0 and operation_stack[-1] != "(":
+                notation.append(operation_stack.pop())
+            operation_stack.pop()  # удаление оставшейся открывающей скобки
 
         # Любой другой случай
         else:
             save_number()
-            print("unusued else triggered, symb is", symb)
+            print("skipping symb", symb)
+        i += 1
 
     # После полного прохода по инфиксному вводу добавляем остатки (упорядоченных) операторов в нотацию
-    while len(operator_stack) > 0:
-        notation.append(operator_stack.pop())
+    while len(operation_stack) > 0:
+        notation.append(operation_stack.pop())
 
     print("Parsing done:")
     print("- Notation is", notation)
-    print("- Operator_stack is", operator_stack)
+    print("- Operator_stack is", operation_stack)
 
-
+# Выполнение операции над одним/двумя операндами (как же хочется eval или хотя бы switch-case...)
 def perform_operation(a, b, operation):
+    print(f"...calculating {a} {operation} {b}")
     if operation == "+":
         return a + b
     elif operation == "-":
@@ -119,7 +177,23 @@ def perform_operation(a, b, operation):
     elif operation == "*":
         return a * b
     elif operation == "/":
-        return int(a / b)
+        return a / b
+    elif operation == "^":
+        return a**b
+    elif operation == "--":
+        return -a
+    elif operation == "sq":
+        return a**0.5
+    elif operation == "ln":
+        return log(radians(a))
+    elif operation == "sn":
+        return sin(radians(a))
+    elif operation == "cs":
+        return cos(radians(a))
+    elif operation == "tn":
+        return tan(radians(a))
+    elif operation == "ct":
+        return cos(radians(a)) / sin(radians(a))
 
 
 def calculate_notation():
@@ -127,13 +201,23 @@ def calculate_notation():
     calc_stack = []  # стек вычислений выражения
 
     for elem in notation:
+        a = 0
+        b = 0
+
         if is_int(elem):
             calc_stack.append(elem)
 
-        elif elem in operators:
+        # Для бинарных операций
+        elif elem in ["*", "^", "/", "+", "-"]:
             b = calc_stack.pop()
             a = calc_stack.pop()
             result = perform_operation(a, b, elem)
+            calc_stack.append(result)
+
+        # Для унарных операций
+        elif elem in ["sq", "ln", "sn", "cs", "tn", "ct"]:
+            a = calc_stack.pop()
+            result = perform_operation(a, 0, elem)
             calc_stack.append(result)
 
     return calc_stack.pop()
@@ -196,20 +280,6 @@ window.geometry(WINDOW_SIZE)
 window.resizable(False, False)
 
 
-# Все ("названия":"внутренние_значения") кнопок. Для каждой создается отдельная кнопка
-# При парсинге изначальной строки названия будут заменяться на внутренние_значения
-# Внутренние значения нужны для простого парсинга операций по двум знакам
-button_labels = [
-    [("Del", "D"), ("(", "("), (")", ")"), ("*", "*")],
-    [("7", "7"), ("8", "8"), ("9", "9"), ("/", "/")],
-    [("4", "4"), ("5", "5"), ("6", "6"), ("-", "-")],
-    [("1", "1"), ("2", "2"), ("3", "3"), ("+", "+")],
-    [("C", "C"), ("0", "0"), ("=", "="), ("|x|", "||")],
-    [("log_e(x)", "le"), ("e^", "e^"), ("**", "**"), ("sqrt", "sq")],
-    [("sin", "sn"), ("cos", "cs"), ("tan", "tn"), ("ctan", "ct")],
-]
-
-
 # Цикл создания кнопок
 for row in range(len(button_labels)):
     for col in range(len(button_labels[row])):
@@ -228,3 +298,6 @@ input_entry.grid(row=1, column=4)
 
 # Вечный цикл отрисовки
 window.mainloop()
+
+# TODO: добавить временный буфер для операций таких как sq, чтобы сначала класть скобки и то что в них, а потом саму операцию
+# TODO: пофиксить унарный минус
