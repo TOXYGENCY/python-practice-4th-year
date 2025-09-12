9b4c8ac1444df9d8eb778a511764fc77dd95a821
TOXYGENCY <delorean2026@ya.ru>
2025-09-12 18:32:44 +0300

    Фикс модуля. Фикс котангенса. Обработка ошибков вычисления. чистка кода. Переделан интерфейс.

--- general_calc.py
+++ general_calc.py
@@ -1,33 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
-from math import log, sin, cos, tan, radians
+from math import e, log, sin, cos, tan, radians
 
 # Константы (poor man's config)
 WINDOW_TITLE = "Калькулятор"
-WINDOW_SIZE = "800x640"
+WINDOW_SIZE = "320x600"
 INPUT_LABEL = "Ввод выражения"
 RESULT_WINDOW_TITLE = "Результат вычисления"
 VALIDATION_ERROR_TITLE = "Ошибка ввода"
-
-
-# Переменные
-
-# Все ("названия":"внутренние_значения") кнопок. Для каждой создается отдельная кнопка
-# При парсинге изначальной строки названия будут заменяться на внутренние_значения
-# Внутренние значения нужны для простого парсинга операций по двум знакам
-button_labels = [
-    [("Del", "D"), ("(", "("), (")", ")"), ("*", "*")],
-    [("7", "7"), ("8", "8"), ("9", "9"), ("/", "/")],
-    [("4", "4"), ("5", "5"), ("6", "6"), ("-", "-")],
-    [("1", "1"), ("2", "2"), ("3", "3"), ("+", "+")],
-    [("C", "C"), ("0", "0"), ("=", "="), ("|x|", "|")],
-    [("log_e(x)", "ln"), ("^", "^"), ("sqrt(x)", "sq"), ("=", "=")],
-    [("sin(x)", "sn"), ("cos(x)", "cs"), ("tan(x)", "tn"), ("ctan(x)", "ct")],
-]
-num_buffer = ""  # Буфер для учета многозначных чисел
-notation = []  # Стек с обратной польской нотацией
-operation_stack = []  # Стек с операторами
-operations = {  # словарь операций и их приоритетов
+CALC_ERROR_TITLE = "Ошибка вычисления"
+CALC_ERROR_TEXT = "Во время вычислений произошла ошибка"
+OPERATIONS = {  # словарь операций и их приоритетов
     "--": 3,  # Явный унарный минус
     "abs": 2,  # модуль
     "^": 2,
@@ -42,6 +25,32 @@ operations = {  # словарь операций и их приоритетов
     "+": 0,
     "-": 0,
 }
+# Все ("названия":"внутренние_значения") кнопок. Для каждой создается отдельная кнопка
+# При парсинге изначальной строки названия будут заменяться на внутренние_значения
+# Внутренние значения нужны для простого парсинга операций по двум знакам
+BUTTON_LABELS = [
+    [("(", "("), (")", ")"), ("^", "^"), ("*", "*")],
+    [("7", "7"), ("8", "8"), ("9", "9"), ("/", "/")],
+    [("4", "4"), ("5", "5"), ("6", "6"), ("-", "-")],
+    [("1", "1"), ("2", "2"), ("3", "3"), ("+", "+")],
+    [("Del", "D"), ("0", "0"), ("=", "="), ("|", "|")],
+    [("C", "C"), ("ln(x)", "ln"), ("=", "="), ("sqrt(x)", "sq")],
+    [("sin(x)", "sn"), ("cos(x)", "cs"), ("tan(x)", "tn"), ("ctan(x)", "ct")],
+]
+BG_COLOR = "#e4eaec"
+FG_COLOR = "black"
+BG_COLOR_BUTTON = "#e2ecf1"
+BG_COLOR_SPECIAL = "#d5b5b9"
+FG_COLOR_SPECIAL = "#3B0009"
+BG_COLOR_NUMBER = "#c5d3e0"
+BG_COLOR_CALC = "#bbd6be"
+FG_COLOR_ACTIVE = "cyan"
+BG_COLOR_ACTIVE = "gray"
+
+# Переменные
+num_buffer = ""  # Буфер для учета многозначных чисел
+notation = []  # Стек с обратной польской нотацией
+operation_stack = []  # Стек с операторами
 
 
 # Проверяет является ли X числом
@@ -79,17 +88,17 @@ def convert_symbols(text):
 
 # Cокращение проверки предыдущ. на спец символ для условий в parse_input()
 def is_special(last):
-    global operations
+    global OPERATIONS
 
     last = str(last)
-    is_oper = str(last) in operations
-    is_bracket = str(last) in ["(", ")", "|"]
+    is_oper = last in OPERATIONS and last != "-"
+    is_bracket = last in ["(", ")", "|"]
     return is_oper or is_bracket
 
 
 # Основная функция парсинга инфиксной записи в ОПН
 def parse_input():
-    global num_buffer, operation_stack, operations, notation
+    global num_buffer, operation_stack, OPERATIONS, notation
     # Очищаем рабочие переменные
     notation, operation_stack, num_buffer = [], [], ""
 
@@ -100,7 +109,6 @@ def parse_input():
     # Из-за изменения строки input, нельзя использовать фиксированный for
     while i < len(input):
         symb = input[i]
-        print("--processing:", symb)
 
         # Числа добавляем в буфер
         if is_int(symb):
@@ -118,7 +126,7 @@ def parse_input():
             or (  # стоит после оператора
                 (0 < i < len(input) - 1)
                 and (symb == "-")
-                and (is_special(input[i - 1]))
+                and (is_special(input[i - 1]) and input[i - 1] != ")")
             )
         ):
             save_number()
@@ -136,21 +144,20 @@ def parse_input():
             operation_stack.append(symb)
 
         # Добавляем операции в стек. Проверяем по двум символам
-        elif i < len(input) - 1 and str(symb + input[i + 1]) in operations:
+        elif i < len(input) - 1 and str(symb + input[i + 1]) in OPERATIONS:
             save_number()
             operation_stack.append(str(symb + input[i + 1]))
 
         # Добавляем операторы в стек в порядке приоритетов
-        elif symb in operations:
+        elif symb in OPERATIONS:
             save_number()
             oper = symb
             # Пока видим операторы в высшим приоритетом - переносим их в нотацию
             while (
                 len(operation_stack) > 0
-                and (
-                    operation_stack[-1] != "(" or operation_stack[-1] != "|"
-                )  # вытаскиваем до откр. скобки или модуля
-                and operations[oper] <= operations[operation_stack[-1]]
+                and operation_stack[-1] != "("
+                and operation_stack[-1] != "|"  # вытаскиваем до откр. скобки или модуля
+                and OPERATIONS[oper] <= OPERATIONS[operation_stack[-1]]  # приоритеты
             ):
                 notation.append(operation_stack.pop())
             operation_stack.append(symb)
@@ -208,7 +215,7 @@ def perform_operation(a, b, operation):
     elif operation == "sq":
         return a**0.5
     elif operation == "ln":
-        return log(radians(a))
+        return log(a)
     elif operation == "sn":
         return sin(radians(a))
     elif operation == "cs":
@@ -216,7 +223,7 @@ def perform_operation(a, b, operation):
     elif operation == "tn":
         return tan(radians(a))
     elif operation == "ct":
-        return cos(radians(a)) / sin(radians(a))
+        return 1 / tan(radians(a))
 
 # Пошаговое вычисление готовой ОПН
 def calculate_notation():
@@ -226,6 +233,7 @@ def calculate_notation():
     i = 0
     while i <= len(notation) - 1:
         elem = notation[i]
+        print("---processing", elem, calc_stack)
         a = 0
         b = 0
 
@@ -242,7 +250,7 @@ def calculate_notation():
         # Для унарных операций
         elif elem in ["--", "sq", "ln", "sn", "cs", "tn", "ct", "abs"]:
             a = calc_stack.pop()
-            result = perform_operation(a, 0, elem)
+            result = perform_operation(a, "none", elem)
             calc_stack.append(result)
 
         i += 1
@@ -253,8 +261,12 @@ def calculate_notation():
 # Функция, запускающая процесс вычисления
 def calculate():
     parse_input()  # делаем нотацию
-    result = str(calculate_notation())
-    messagebox.showinfo(RESULT_WINDOW_TITLE, result)
+    try:
+        result = str(calculate_notation())
+        messagebox.showinfo(RESULT_WINDOW_TITLE, result)
+    except Exception as ex:
+        messagebox.showerror(CALC_ERROR_TITLE, CALC_ERROR_TEXT)
+        raise ex
 
 
 # Функция с валидациями ввода. Возвращает кортеж из результата и сообщения ошибки
@@ -291,6 +303,17 @@ def button_click(text, value):
 
 # Функция создания кнопок
 def create_button(text, value, row, col):
+    bg_color = BG_COLOR_BUTTON
+    fg_color = FG_COLOR
+
+    if value in ["C", "D"]:
+        bg_color = BG_COLOR_SPECIAL
+        fg_color = FG_COLOR_SPECIAL
+    elif is_int(value):
+        bg_color = BG_COLOR_NUMBER
+    elif value == "=":
+        bg_color = BG_COLOR_CALC
+
     button = Button(
         window,
         text=text,
@@ -298,8 +321,13 @@ def create_button(text, value, row, col):
         width=5,
         height=2,
         font=("Arial", 16),
+        activeforeground=FG_COLOR_ACTIVE,
+        activebackground=BG_COLOR_ACTIVE,
+        cursor="hand2",
+        background=bg_color,
+        foreground=fg_color,
     )
-    button.grid(row=row, column=col, padx=5, pady=5)
+    button.grid(row=row, column=col, padx=4, pady=4)
     return button
 
 
@@ -308,25 +336,27 @@ window = Tk()
 window.title(WINDOW_TITLE)
 window.geometry(WINDOW_SIZE)
 window.resizable(False, False)
-
+window.configure(background=BG_COLOR)
 
 # Цикл создания кнопок
-for row in range(len(button_labels)):
-    for col in range(len(button_labels[row])):
+column_offset = 0
+row_offset = 2
+for row in range(len(BUTTON_LABELS)):
+    for col in range(len(BUTTON_LABELS[row])):
         button = create_button(
-            button_labels[row][col][0], button_labels[row][col][1], row, col
+            BUTTON_LABELS[row][col][0],
+            BUTTON_LABELS[row][col][1],
+            row + row_offset,
+            col + column_offset,
         )
 
 
 # Поля ввода
-input_label = Label(window, text=INPUT_LABEL, font=("Arial", 16))
-input_label.grid(row=0, column=4)
+input_label = Label(window, text=INPUT_LABEL, font=("Arial", 14), background=BG_COLOR)
+input_label.grid(row=0, column=0, columnspan=4, padx=5)
 
-input_entry = Entry(window, width=40, font=("Arial", 14))
-input_entry.grid(row=1, column=4)
-
+input_entry = Entry(window, width=27, font=("Arial", 14))
+input_entry.grid(row=1, column=0, columnspan=4, pady=8)
 
 # Вечный цикл отрисовки
 window.mainloop()
-
-# TODO: добавить модуль
